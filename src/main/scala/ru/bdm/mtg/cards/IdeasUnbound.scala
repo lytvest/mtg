864efb8491e9071178df03d620f1c6bc9a1481de
AceSpade <313121@mail.ru>
2020-09-22 19:00:17 +0500

    Tried to fix some bugs, but no idea how to do it, so Dimasta de Code masta will take care of of it

--- src/main/scala/ru/bdm/mtg/cards/IdeasUnbound.scala
+++ src/main/scala/ru/bdm/mtg/cards/IdeasUnbound.scala
@@ -1,14 +1,14 @@
 package ru.bdm.mtg.cards
 
 import ru.bdm.mtg.Card
-import ru.bdm.mtg.actions.{Action, AddDiscard, AddEndTurnDiscard, RemoveFromHand, TakeCards}
+import ru.bdm.mtg.actions.{Action, AddDiscard, AddEndTurnDiscard, RemoveFromHand, RemoveFromHandAndMana, TakeCards}
 import ru.bdm.mtg.conditions.{Condition, Discard, InHand, IsPlay, IsPlayFromHandAndMana}
 
 class IdeasUnbound extends Card {
   val cost = "UU"
 
   override val description: Map[Condition, Action] = Map(
-    IsPlayFromHandAndMana(this, cost) -> AddEndTurnDiscard(3) * TakeCards(3) * RemoveFromHand(this),
+    IsPlayFromHandAndMana(this, cost) -> AddEndTurnDiscard(3) * TakeCards(3) * RemoveFromHandAndMana(this, cost),
     Discard.standard(this)
   )
 }
